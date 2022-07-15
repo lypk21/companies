9ee54eb877e371845b567c73cd81082233069b5c
Liu Louis <lypk21@gmail.com>
2022-07-15 12:20:57 +1000

    unit test done

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-1. composer dump-autoload
+This small project mainly do the following tasks:
+
+• Basic Laravel Auth: ability to log in as administrator
+
+• Use database seeds to create first user with email admin@admin.com and password “password”
+
+• CRUD functionality (Create / Read / Update / Delete) for two menu items: Companies and Employees.
+
+• Companies DB table consists of these fields: Name (required), email, logo (minimum 100×100), website
+
+• Employees DB table consists of these fields: First name (required), last name (required), Company (foreign key to Companies), email, phone
+
+• Use database migrations to create those schemas above
+
+• Use database seeds to create at least 10 initial companies
+
+• Store companies logos in storage/app/public folder and make them accessible from public
+
+• Use basic Laravel resource controllers with default methods – index, create, store etc.
+
+• Use Laravel’s validation function, using Request classes
+
+• Use Laravel’s pagination for showing Companies/Employees list, 10 entries per page
+
+• Create authentication without using Laravel fortify.
+
+• Email notification: send email whenever new company is entered (use Mailgun or Mailtrap)
+
+• Testing with phpunit
+
+how to run the project?
+
+• php artisan migrate
+
+• php artisan db:seed
+
+• composer dump-autoload
+
+About the short project:
+
+1. build with laravel 8, the structure mainly import the services layer to handle business logic for decoupling the controller.
+2. create middle middle ware for user and only administrator and owner can access the backend.
+3. apply the laravel best practice principle 
+4. have some http unit test cases, but not fully cover due to limited space time.
--- tests/Feature/CompanyTest.php
+++ tests/Feature/CompanyTest.php
@@ -2,10 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Http\Utiles\Constants;
 use App\Models\Company;
-use Database\Factories\CompanyFactory;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class CompanyTest extends TestCase
@@ -15,14 +15,65 @@ class CompanyTest extends TestCase
      *
      * @return void
      */
-    public function test_companies_list_url()
+
+    public function getAdminUser() {
+        return User::where('role', Constants::USER_ROLE_ADMINISTRATOR)->first();
+    }
+
+    public function test_index_authentication()
     {
-        $response = $this->get('/admin/company');
-        $response->assertStatus(200);
+        $this->actingAs($this->getAdminUser());
+        $company = Company::first();
+        $this->get('/admin/company')->assertStatus(200);
+        $this->get('/admin/company/create')->assertStatus(200);
+        $this->get('/admin/company/'.$company->id.'/edit')->assertStatus(200);
+
     }
-    public function test_a_user_can_read_all_the_companies() {
-        $company =  Company::factory(1)->create();;
-        $response = $this->get('/admin/company');
-        $response->assertSee($company->name);
+
+
+    public function test_admin_visit_companies_list()
+    {
+        $response = $this->actingAs($this->getAdminUser())->get('/admin/company');
+        $response->assertStatus(200)
+                ->assertSee('Company List');
     }
+
+    public function test_admin_create_companies() {
+
+        $this->actingAs($this->getAdminUser());
+
+        $this->get('/admin/company/create')
+            ->assertStatus(200)
+            ->assertViewIs('admin.company.create')
+            ->assertSee("Create Company");
+
+        $this->post('/admin/company/create',
+            [
+                'name' => Str::random(10),
+                '_token' => csrf_token(),
+                "email" => null,
+                "website" => null
+            ]
+        )->assertSee("create company successfully");
+    }
+
+    public function  test_admin_edit_companies() {
+        $this->actingAs($this->getAdminUser());
+        $company = Company::first();
+
+        $this->get('/admin/company/'.$company->id.'/edit')
+            ->assertStatus(200)
+            ->assertViewIs('admin.company.edit')
+            ->assertSee("Edit Company");
+
+        $this->put('/admin/company/'.$company->id,
+            [
+                'name' => Str::random(10),
+                '_token' => csrf_token(),
+            ]
+        )->assertRedirect('/admin/company')
+        ->assertSessionHas('success');
+
+    }
+
 }
